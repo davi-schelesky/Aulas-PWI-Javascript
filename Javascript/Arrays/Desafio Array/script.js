2b17a79a4686d2ff84bc2c20f9d1f135ba7500a2
davi-schelesky <davischelesky2011@gmail.com>
2026-09-08 12:50:12 -0300

    Finalizando o desafio array

--- Javascript/Arrays/Desafio Array/script.js
+++ Javascript/Arrays/Desafio Array/script.js
@@ -2,7 +2,9 @@ const produtos = [
     ['Notebook Dell', 'Notebook Dell I7 14" 500GB 16RAM', 'Notebook', '7600.99', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh8TQQAQnnYWxPDgtV4pOwFhiePF6-e_wwDJm3hSfv4g&s=10'],
     ['Notebook Acer', 'Notebook Acer I5 11" 256GB 16RAM', 'Notebook', '3500.00', 'https://m.media-amazon.com/images/I/51cj43d2FUL._AC_UF894,1000_QL80_.jpg'],
     ['Notebook Asus', 'Notebook Asus I9 14" 1TB 32RAM', 'Notebook', '9800.00', 'https://m.media-amazon.com/images/I/61O9g8YnSkL._AC_UF894,1000_QL80_.jpg'],
-    ['Celular Iphone', 'Celular Iphone 17 500GB 8RAM', 'Celular', '7300.59', 'https://www.apple.com/v/iphone-17-pro/g/images/meta/iphone-17-pro_overview__eumhhclcpuaa_og.png?202607160844']
+    ['Celular Iphone', 'Celular Iphone 17 500GB 8RAM', 'Celular', '7300.59', 'https://www.apple.com/v/iphone-17-pro/g/images/meta/iphone-17-pro_overview__eumhhclcpuaa_og.png?202607160844'],
+    ['Playstation 5', 'PS5 Slim Wolverine Edition', 'Videogame', '4300', 'https://images.kabum.com.br/produtos/fotos/1065184/console-playstation-5-sony-edicao-digital-marvel-s-wolverine-pacote-de-edicao-limitada-825-gb-amarelo-1000056865_1787145891_gg.jpg'],
+    ['PC Gamer Completo', 'PC Gamer Ryzen 7 7600', 'PC', '6400', 'https://images.kabum.com.br/produtos/fotos/sync_mirakl/684007/xlarge/PC-Gamer-Completo-Ryzen-5-5600gt-16gb-Ddr4-SSD-480GB-500w-80-Plus-PCgt13-e_1779735227.png']
 ];
 const sectionCard = document.querySelector(".cards");
 const inputEnviar = document.getElementById('inputEnviar');
@@ -33,11 +35,21 @@ function criarCard(categoria){
 
 function procurarItem(e){
     e.preventDefault();
-    const itemProcurado = document.getElementById('pesquisa').value;
+    const inputPesquisa = document.getElementById('pesquisa');
+    const itemProcurado = inputPesquisa.value.trim();
 
-    let categoria = produtos.filter((produto) => (produto[2].toLowerCase() == itemProcurado.toLowerCase().trim()));
-    console.log(categoria);
-    criarCard(categoria);
+    let categoria = produtos.filter((produto) => (produto[2].toLowerCase() == itemProcurado.toLowerCase()));
+
+    if(categoria.length == 0){
+        sectionCard.innerHTML = '';
+        const textoSemItens = document.createElement("p");
+        textoSemItens.textContent = "Não há essses itens no catálogo";
+        textoSemItens.style = "color: red; font-size: 2rem;";
+        sectionCard.appendChild(textoSemItens);
+    } else {
+        criarCard(categoria);
+    }
+    inputPesquisa.value = "";
 }
 
 criarCard(produtos);
