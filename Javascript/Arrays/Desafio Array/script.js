b21e0101943ef2dbfba43fd5aac198f49261b013
davi-schelesky <davischelesky2011@gmail.com>
2026-09-09 11:56:35 -0300

    Melhoria do desafio array

--- Javascript/Arrays/Desafio Array/script.js
+++ Javascript/Arrays/Desafio Array/script.js
@@ -4,7 +4,9 @@ const produtos = [
     ['Notebook Asus', 'Notebook Asus I9 14" 1TB 32RAM', 'Notebook', '9800.00', 'https://m.media-amazon.com/images/I/61O9g8YnSkL._AC_UF894,1000_QL80_.jpg'],
     ['Celular Iphone', 'Celular Iphone 17 500GB 8RAM', 'Celular', '7300.59', 'https://www.apple.com/v/iphone-17-pro/g/images/meta/iphone-17-pro_overview__eumhhclcpuaa_og.png?202607160844'],
     ['Playstation 5', 'PS5 Slim Wolverine Edition', 'Videogame', '4300', 'https://images.kabum.com.br/produtos/fotos/1065184/console-playstation-5-sony-edicao-digital-marvel-s-wolverine-pacote-de-edicao-limitada-825-gb-amarelo-1000056865_1787145891_gg.jpg'],
-    ['PC Gamer Completo', 'PC Gamer Ryzen 7 7600', 'PC', '6400', 'https://images.kabum.com.br/produtos/fotos/sync_mirakl/684007/xlarge/PC-Gamer-Completo-Ryzen-5-5600gt-16gb-Ddr4-SSD-480GB-500w-80-Plus-PCgt13-e_1779735227.png']
+    ['PC Gamer Completo', 'PC Gamer Ryzen 7 7600', 'PC', '6400', 'https://images.kabum.com.br/produtos/fotos/sync_mirakl/684007/xlarge/PC-Gamer-Completo-Ryzen-5-5600gt-16gb-Ddr4-SSD-480GB-500w-80-Plus-PCgt13-e_1779735227.png'],
+    ['Nintendo Switch Oled', 'Nintendo Switch', 'Videogame', '3200', 'https://m.media-amazon.com/images/I/61nqNujSF2L.jpg'],
+    ['Mouse Gamer Logitech', 'Mouse Gamer Logitech', 'Periféricos', '220', 'https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/oficinadosbits/media/uploads/produtos/foto/vztogtli/file.png']
 ];
 const sectionCard = document.querySelector(".cards");
 const inputEnviar = document.getElementById('inputEnviar');
